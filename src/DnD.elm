5b2f39708f273f1a95adbf8fb7489e59a169cce1
Kenni Bawden <kenni.bawden@gmail.com>
2016-05-03 13:32:45 +1000

    Added simple skills and character data type

--- src/DnD.elm
+++ src/DnD.elm
@@ -4,14 +4,43 @@ import Graphics.Element exposing (show)
 
 import DnD.Data as DnD
 
-main = show abilities
+main = show character
+
+character : DnD.Character
+character =
+    { level = 1
+    , abilities = abilities
+    , skills = skills
+    }
 
 abilities : DnD.Abilities
-abilities = DnD.Abilities
+abilities =
     { strength = 15
     , dexterity = 14
     , constitution = 12
     , intelligence = 10
     , wisdom = 13
     , charisma = 8
+    }
+
+skills : DnD.Skills
+skills =
+    { athletics = True
+    , acrobatics = True
+    , sleightOfHand = False
+    , stealth = True
+    , arcana = False
+    , history = False
+    , investigation = False
+    , nature = False
+    , religion = False
+    , animalHandling = False
+    , insight = True
+    , medicine = False
+    , perception = False
+    , survival = True
+    , deception = False
+    , intimidation = False
+    , performance = False
+    , persuasion = False
     }
--- src/DnD/Data.elm
+++ src/DnD/Data.elm
@@ -1,12 +1,41 @@
 module DnD.Data
-    ( Abilities(..)
+    ( Character
+    , Abilities
+    , Skills
     ) where
-    
-type Abilities = Abilities
+
+type alias Character =
+    { level : Int
+    , abilities : Abilities
+    , skills : Skills
+    }
+
+type alias Abilities =
     { strength : Int
     , dexterity : Int
     , constitution : Int
     , intelligence : Int
     , wisdom : Int
     , charisma : Int
+    }
+
+type alias Skills =
+    { athletics : Bool
+    , acrobatics : Bool
+    , sleightOfHand : Bool
+    , stealth : Bool
+    , arcana : Bool
+    , history : Bool
+    , investigation : Bool
+    , nature : Bool
+    , religion : Bool
+    , animalHandling : Bool
+    , insight : Bool
+    , medicine : Bool
+    , perception : Bool
+    , survival : Bool
+    , deception : Bool
+    , intimidation : Bool
+    , performance : Bool
+    , persuasion : Bool
     }
